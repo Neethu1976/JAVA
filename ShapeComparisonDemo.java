c28247e4f94aecfaa82d6103ef20149b10389dd8
Neethu Seby P <91589410+Neethu1976@users.noreply.github.com>
2025-02-12 23:40:17 +0530

    Add files via upload

---
+++ ShapeComparisonDemo.java
@@ -0,0 +1,96 @@
+ import java.util.Scanner;
+
+interface ShapeComparable {
+    double volume();
+    double area();
+    int compareByVolume(ShapeComparable other);
+    int compareByArea(ShapeComparable other);
+}
+
+class Cuboid implements ShapeComparable {
+    double length, width, height;
+
+    public Cuboid(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double volume() {
+        return length * width * height;
+    }
+
+    public double area() {
+        return 2 * (length * width + width * height + height * length);
+    }
+
+    public int compareByVolume(ShapeComparable other) {
+        return Double.compare(this.volume(), other.volume());
+    }
+
+    public int compareByArea(ShapeComparable other) {
+        return Double.compare(this.area(), other.area());
+    }
+}
+
+class Cylinder implements ShapeComparable {
+    double radius, height;
+
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    public double area() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    public int compareByVolume(ShapeComparable other) {
+        return Double.compare(this.volume(), other.volume());
+    }
+
+    public int compareByArea(ShapeComparable other) {
+        return Double.compare(this.area(), other.area());
+    }
+}
+
+public class ShapeComparisonDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.println("Enter the cuboid's length, width, and height:");
+        double length = sc.nextDouble();
+        double width = sc.nextDouble();
+        double height = sc.nextDouble();
+        Cuboid cuboid = new Cuboid(length, width, height);
+
+       
+        System.out.println("Enter the cylinder's radius and height:");
+        double radius = sc.nextDouble();
+        double cylinderHeight = sc.nextDouble();
+        Cylinder cylinder = new Cylinder(radius, cylinderHeight);
+
+      
+        System.out.println("\nCuboid Volume: " + cuboid.volume());
+        System.out.println("Cylinder Volume: " + cylinder.volume());
+
+       
+        int volumeComparison = cuboid.compareByVolume(cylinder);
+        if (volumeComparison > 0) {
+            System.out.println("Cuboid has a larger volume than the cylinder.");
+        } else if (volumeComparison < 0) {
+            System.out.println("Cylinder has a larger volume than the cuboid.");
+        } else {
+            System.out.println("Both shapes have the same volume.");
+        }
+
+     
+
+        sc.close();
+    }
+} 
